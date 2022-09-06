4c240433c4404bce8ee71ebc1bb9bb876571484a
Diy1990 <leeo.av@live.com>
2022-09-06 17:40:21 -0600

    homework

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroIf = 3;
+
+
+        if (numeroIf < 0) {
+            System.out.println("el numero es negativo");
+
+
+        } else if (numeroIf > 0) {
+            System.out.println("el numero es postivo");
+        }else {
+            System.out.println("el numero es positivo");
+        }
+
+
+
+    }
+}
